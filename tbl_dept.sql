0073fdcb53efc340c1ef3aecaf0baf91ec703711
Ai-DING <1941784123@qq.com>
2020-07-25 13:10:59 +0800

    first commit

---
+++ tbl_dept.sql
@@ -0,0 +1,6 @@
+INSERT INTO `tbl_dept`(`dept_id`, `dept_name`) VALUES (1, '云南省');
+INSERT INTO `tbl_dept`(`dept_id`, `dept_name`) VALUES (2, '广东省');
+INSERT INTO `tbl_dept`(`dept_id`, `dept_name`) VALUES (3, '湖南省');
+INSERT INTO `tbl_dept`(`dept_id`, `dept_name`) VALUES (4, '湖北省');
+INSERT INTO `tbl_dept`(`dept_id`, `dept_name`) VALUES (5, '甘肃省');
+INSERT INTO `tbl_dept`(`dept_id`, `dept_name`) VALUES (6, '总务处');
